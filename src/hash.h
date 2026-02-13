0ce1914b67ae539d691ac5189014d20559923dab
liaolihui <liaolihui00@gmail.com>
2026-02-13 23:22:08 +0800

    对部分头文件进行了注释，方便理解代码

--- src/hash.h
+++ src/hash.h
@@ -8,25 +8,25 @@
 #ifndef hash_h
 #define hash_h
 
-typedef struct node
+typedef struct node//Node结构体，hash表的每个节点
 {
- Object ref;
- Object val;
- struct node *next;
+ Object ref;//key键值
+ Object val;//value值
+ struct node *next;//指向下一个节点的指针，形成链表
 } Node;
 
-typedef struct Hash
+typedef struct Hash//Hash结构体
 {
- char           mark;
- unsigned int   nhash;
- Node         **list;
+ char           mark;//标记，表示这个hash表是否在使用
+ unsigned int   nhash;//hash表的大小，即hash表里有多少个桶
+ Node         **list;//指向Node指针的指针，list是一个数组，每个元素都是一个Node指针，指向该桶的链表头节点
 } Hash;
 
 
-Hash    *lua_createarray (int nhash);
-void     lua_hashmark (Hash *h);
-void     lua_hashcollector (void);
-Object 	*lua_hashdefine (Hash *t, Object *ref);
-void     lua_next (void);
+Hash    *lua_createarray (int nhash);//创建一个新的hash表，hash表里有nhash个桶
+void     lua_hashmark (Hash *h);//mark hash表还在使用
+void     lua_hashcollector (void);//hashcollector回收hash表里没有被mark即不再使用的hash表
+Object 	*lua_hashdefine (Hash *t, Object *ref);//在表 t 中为键 ref 寻找或创建一个位置，返回该位置的指针
+void     lua_next (void);//迭代器，在hash表里返回下一个键值对
 
 #endif
--- src/table.h
+++ src/table.h
@@ -7,36 +7,39 @@
 #ifndef table_h
 #define table_h
 
-extern Symbol *lua_table;
-extern Word    lua_ntable;
+extern Symbol *lua_table;//符号表，记录程序里的变量名
+extern Word    lua_ntable;//当前符号表里有多少个符号
 
-extern char  **lua_constant;
-extern Word    lua_nconstant;
+extern char  **lua_constant;//常量表，记录程序里的常量
+extern Word    lua_nconstant;//常量总个数
 
-extern char  **lua_string;
-extern Word    lua_nstring;
+extern char  **lua_string;//字符串表，记录程序里的字符串
+extern Word    lua_nstring;//字符串总个数，同样的字符串只记录一次
 
-extern Hash  **lua_array;
-extern Word    lua_narray;
+extern Hash  **lua_array;//hash表数组，记录程序里的数组
+extern Word    lua_narray;//hash表总个数
 
-extern char   *lua_file[];
-extern int     lua_nfile;
+extern char   *lua_file[];//文件名表，记录程序里用到的.lua源码文件名
+extern int     lua_nfile;//当前文件名表里有多少个文件名
 
-extern Word    lua_block;
+extern Word    lua_block;//内存块管理
 extern Word    lua_nentity;
 
 
 
-int   lua_findsymbol      (char *s);
-int   lua_findconstant    (char *s);
-void  lua_travsymbol      (void (*fn)(Object *));
-void  lua_markobject      (Object *o);
-void  lua_pack            (void);
-void  lua_stringcollector (void);
-char *lua_createstring    (char *s);
-int   lua_addfile         (char *fn);
-int   lua_delfile 	  (void);
-char *lua_filename        (void);
-void  lua_nextvar         (void);
+int   lua_findsymbol      (char *s);//在符号表里查找符号s对应的索引
+int   lua_findconstant    (char *s);//在常量表里查找常量s对应的索引
+
+void  lua_travsymbol      (void (*fn)(Object *));//遍历符号表里所有的对象，并对每个对象调用函数fn
+void  lua_markobject      (Object *o);//mark对象还在使用
+void  lua_pack            (void);//pack整理内存空间
+void  lua_stringcollector (void);//stringcollecor回收字符串表里不再使用的字符串
+
+char *lua_createstring    (char *s);//创建字符串s，如果s已经存在于字符串表里，返回字符串表里对应的字符串
+int   lua_addfile         (char *fn);//源码文件栈管理，在文件名表里添加文件名fn
+int   lua_delfile 	  (void);//从文件名表里删除最后一个文件名
+char *lua_filename        (void);//返回当前正在处理的文件名,主要用于报错
+
+void  lua_nextvar         (void);//变量迭代器，在符号表里返回下一个符号和对应的对象
 
 #endif
