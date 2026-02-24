6d068e71449a042014cf485fdc4afa763dfe48ae
liaolihui <liaolihui00@gmail.com>
2026-02-24 16:58:53 +0800

    修改Makefile和部分源文件，使其能够在现代的Linux系统上运行

--- Makefile
+++ Makefile
@@ -1,11 +1,25 @@
-# makefile for lua hierarchy
+# # makefile for lua hierarchy
+
+# all:
+# 	(cd src; make)
+# 	(cd clients/lib; make)
+# 	(cd clients/lua; make)
+
+# clean:
+# 	(cd src; make clean)
+# 	(cd clients/lib; make clean)
+# 	(cd clients/lua; make clean)
+
+
+LUA ?= $(PWD)
+export LUA
 
 all:
-	(cd src; make)
-	(cd clients/lib; make)
-	(cd clients/lua; make)
+	(cd src; $(MAKE))
+	(cd clients/lib; $(MAKE))
+	(cd clients/lua; $(MAKE))
 
 clean:
-	(cd src; make clean)
-	(cd clients/lib; make clean)
-	(cd clients/lua; make clean)
+	(cd src; $(MAKE) clean)
+	(cd clients/lib; $(MAKE) clean)
+	(cd clients/lua; $(MAKE) clean)
--- clients/lua/lua.c
+++ clients/lua/lua.c
@@ -10,7 +10,7 @@ char *rcs_lua="$Id: lua.c,v 1.1 1993/12/17 18:41:19 celes Exp $";
 #include "lua.h"
 #include "lualib.h"
 
-void main (int argc, char *argv[])//程序入口函数，接受命令行参数argc和argv，初始化Lua标准库，并根据命令行参数执行相应的Lua代码，如果没有参数则进入交互模式，读取用户输入的Lua代码并执行
+int main (int argc, char *argv[])//程序入口函数，接受命令行参数argc和argv，初始化Lua标准库，并根据命令行参数执行相应的Lua代码，如果没有参数则进入交互模式，读取用户输入的Lua代码并执行
 {
  int i;
  iolib_open ();//初始化输入输出库，提供文件操作和标准输入输出功能
@@ -19,10 +19,11 @@ void main (int argc, char *argv[])//程序入口函数，接受命令行参数ar
  if (argc < 2)//如果没有命令行参数，进入交互模式，读取用户输入的Lua代码并执行
  {
    char buffer[2048];
-   while (gets(buffer) != 0)
+   while (fgets(buffer,sizeof(buffer),stdin) != NULL)
      lua_dostring(buffer);
  }
  else//如果有命令行参数，依次执行每个参数指定的Lua文件
    for (i=1; i<argc; i++)
     lua_dofile (argv[i]);
+  return 0;
 }
